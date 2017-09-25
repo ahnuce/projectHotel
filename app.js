e1d8c3b3e2bdf03f9852a9328d967829c03cb239
Anusone Nanthavongsa <ahnuce@gmail.com>
2017-09-25 19:59:11 -0400

    splitting up javascript so one file doesnt run it all

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ require('./instantHello');
 var goodbye = require('./talk/goodbye');
 //if you have an index.js within a folder you dont have specify the name of the file itself you just need the name of the parent folder
 var talk = require('./talk');
+var question = require('./talk/question');
+
 talk.intro();
 talk.hello('Anusone');
+
+var answer = question.ask("what is the meaning of life?");
+console.log(answer);
+
 goodbye();
