bdd8d2fd73c23c189d95046b82b70ae091aae924
Anusone Nanthavongsa <ahnuce@gmail.com>
2017-10-04 15:06:55 -0400

    setup app.get to be able to request and respond with data usingthe browser

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var app = express();
 
 app.set('port', 9000);
 
+app.get('/', function(req, res){
+    console.log("GET the homepage");
+    res.send("This is the response youre going to get from the res.send");
+});
+
 var server = app.listen(app.get('port'), function(){
     var port = server.address().port;
     console.log("Power level is at port " + port);  
 });
-console.log('Me first');
--- meanStackNotesExpress.js
+++ meanStackNotesExpress.js
@@ -19,3 +19,17 @@ console.log('Me first');
 
 //you then set the app.listen method to a variable to be able to access methods and properties from this callback function 
 //the var port = server.address().port allows you to access the port number without using app.get in 
+//app.listen is asynchronious
+
+//for routes we need to specify http method like a get or a post we need specify a path and specify the function we want to run when the route is found 
+
+app.get('/', function(req, res){
+    console.log("GET the homepage");
+});
+
+//the function in the route usually takes 2 objects, a request (req) and a response (res)
+app.get('/', function(req, res){
+    console.log("GET the homepage");
+    res.send("This is the response youre going to get from the res.send");
+});
+//this res.send response.send is what you get when you get a request from the browser
